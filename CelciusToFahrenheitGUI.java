87b59d26b1a26938bf66a55942e5c2fcb962231a
JessicaFransisca51017012 <48231202+JessicaFransisca51017012@users.noreply.github.com>
2019-11-14 12:09:43 +0800

    Add files via upload

--- CelciusToFahrenheitGUI.java
+++ CelciusToFahrenheitGUI.java
@@ -32,7 +32,6 @@ public class CelciusToFahrenheitGUI extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jLabel2 = new javax.swing.JLabel();
         Reamur = new javax.swing.JLabel();
-        Convertreamur = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -54,28 +53,21 @@ public class CelciusToFahrenheitGUI extends javax.swing.JFrame {
             }
         });
 
-        Convertreamur.setText("Convert");
-        Convertreamur.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                ConvertreamurActionPerformed(evt);
-            }
-        });
-
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                    .addComponent(celciustextfield, javax.swing.GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
-                    .addComponent(convertbutton, javax.swing.GroupLayout.DEFAULT_SIZE, 101, Short.MAX_VALUE)
-                    .addComponent(Convertreamur, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(43, 43, 43)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(Reamur)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel1))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addComponent(celciustextfield, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(43, 43, 43)
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(Reamur)
+                            .addComponent(jLabel2)
+                            .addComponent(jLabel1)))
+                    .addComponent(convertbutton, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(0, 174, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
@@ -86,14 +78,12 @@ public class CelciusToFahrenheitGUI extends javax.swing.JFrame {
                     .addComponent(celciustextfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel1))
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(convertbutton)
-                    .addComponent(jLabel2))
+                .addComponent(jLabel2)
                 .addGap(18, 18, 18)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(Reamur)
-                    .addComponent(Convertreamur))
-                .addContainerGap(165, Short.MAX_VALUE))
+                .addComponent(Reamur)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(convertbutton)
+                .addContainerGap(149, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -131,11 +121,6 @@ public class CelciusToFahrenheitGUI extends javax.swing.JFrame {
             // TODO add your handling code here:
     }//GEN-LAST:event_ReamurMouseClicked
 
-    private void ConvertreamurActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ConvertreamurActionPerformed
-        // TODO add your handling code here:
-        
-    }//GEN-LAST:event_ConvertreamurActionPerformed
-
     /**
      * @param args the command line arguments
      */
@@ -172,7 +157,6 @@ public class CelciusToFahrenheitGUI extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton Convertreamur;
     private javax.swing.JLabel Reamur;
     private javax.swing.JTextField celciustextfield;
     private javax.swing.JButton convertbutton;
